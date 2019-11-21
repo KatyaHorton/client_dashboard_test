8bf19b344b820adf3016362134be2901b3d60cfd
Katya_Horton <you@example.com>
2019-11-21 12:03:04 +0800

    chore: create main colponents

---
+++ src/components/DashboardComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const DashboardComponent = () => {
+  return (
+    <article>
+      <header>Dashboard</header>
+      <div>Here is the Dashboard</div>
+      <footer>
+        Dashboard for <em>Clicks Game</em>
+      </footer>
+    </article>
+  );
+};
