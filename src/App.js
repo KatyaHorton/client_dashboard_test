db6595d6f310193ec05ba88b6fb73be09fee9195
Katya_Horton <you@example.com>
2019-11-21 12:17:47 +0800

    chore: create router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import Route from "react-router-dom/Route";
+
+import { DashboardComponent } from "./components/DashboardComponent";
+import { ClientComponent } from "./components/ClientComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Router>
+      <Route
+        exact
+        path="/"
+        render={() => {
+          return (
+            <article>
+              <h1>Hello, Client Dashboard</h1>
+              <ul>
+                <li>
+                  <Link to="/client">To CLIENT App</Link>
+                </li>
+                <li>
+                  <Link to="/dashboard">To DASHBOARD App</Link>
+                </li>
+              </ul>
+            </article>
+          );
+        }}
+      />
+      <Route path="/client" component={ClientComponent} />
+      <Route path="/dashboard" component={DashboardComponent} />
+    </Router>
   );
 }
 
