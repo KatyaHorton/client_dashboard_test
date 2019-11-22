519e886ebbaaae68e45fa2709eaca801a659a583
Katya_Horton <you@example.com>
2019-11-22 12:56:01 +0800

    chore: add calculations

--- src/context/ContextProvider.tsx
+++ src/context/ContextProvider.tsx
@@ -28,20 +28,30 @@ export const Context = React.createContext<AppContextInterface>({
 
 export const ContextProvider = ({ children }: ProviderProps) => {
   const [gameOn, setGameOn] = React.useState(false);
+  const [startTime, setStartTime] = React.useState(0);
   const [orangeData, setOrangeData] = React.useState([{ clicks: 0, time: 0 }]);
   const [blueData, setBlueData] = React.useState([{ clicks: 0, time: 0 }]);
 
   const startGame = () => {
     setGameOn(true);
+    setStartTime(Date.now());
     setTimeout(() => setGameOn(false), 5000);
     setOrangeData([{ clicks: 0, time: 0 }]);
     setBlueData([{ clicks: 0, time: 0 }]);
   };
 
+  const setClickTime = () => {
+    let time;
+    time = Date.now() - startTime;
+    time = time / 1000;
+    time = Math.round(time * 2) / 2;
+    return time;
+  };
+
   const setData = (isOrange: boolean) => {
     isOrange
-      ? setOrangeData([...orangeData, { clicks: 1, time: Date.now() }])
-      : setBlueData([...blueData, { clicks: 1, time: Date.now() }]);
+      ? setOrangeData([...orangeData, { clicks: 1, time: setClickTime() }])
+      : setBlueData([...blueData, { clicks: 1, time: setClickTime() }]);
     console.log("orangeData", orangeData);
     console.log("blueData", blueData);
   };
