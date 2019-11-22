3f8e95a31c69dee53fda6a78a82bfdb74c3b5cb2
Katya_Horton <you@example.com>
2019-11-22 12:09:10 +0800

    chore: add setting data

--- src/context/ContextProvider.tsx
+++ src/context/ContextProvider.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
 
+export interface ClicksData {
+  clicks: number;
+  time: number;
+}
+
 export interface AppContextInterface {
-  orangeClicks: number;
-  orangeTime: number;
-  blueClicks: number;
-  blueTime: number;
+  orangeData: ClicksData;
+  blueData: ClicksData;
   gameOn: boolean;
   startGame: () => void;
+  onClick: (isOrange: boolean) => void;
 }
 
 interface ProviderProps {
@@ -15,29 +19,40 @@ interface ProviderProps {
 }
 
 export const Context = React.createContext<AppContextInterface>({
-  orangeClicks: 0,
-  orangeTime: 0,
-  blueClicks: 0,
-  blueTime: 0,
+  orangeData: { clicks: 0, time: 0 },
+  blueData: { clicks: 0, time: 0 },
   gameOn: false,
-  startGame: () => {}
+  startGame: () => {},
+  onClick: (isOrange: boolean) => {}
 });
 
 export const ContextProvider = ({ children }: ProviderProps) => {
   const [gameOn, setGameOn] = React.useState(false);
+  const [orangeData, setOrangeData] = React.useState({ clicks: 0, time: 0 });
+  const [blueData, setBlueData] = React.useState({ clicks: 0, time: 0 });
+
   const startGame = () => {
     setGameOn(true);
     setTimeout(() => setGameOn(false), 5000);
+    setOrangeData({ clicks: 0, time: 0 });
+    setBlueData({ clicks: 0, time: 0 });
+  };
+
+  const setData = (isOrange: boolean) => {
+    isOrange
+      ? setOrangeData({ clicks: orangeData.clicks + 1, time: Date.now() })
+      : setBlueData({ clicks: blueData.clicks + 1, time: Date.now() });
+    console.log("orangeData", orangeData);
+    console.log("blueData", blueData);
   };
 
   return (
     <Context.Provider
       value={{
-        orangeClicks: 0,
-        orangeTime: 0,
-        blueClicks: 0,
-        blueTime: 0,
+        orangeData: orangeData,
+        blueData: blueData,
         startGame: startGame,
+        onClick: setData,
         gameOn: gameOn
       }}
     >
